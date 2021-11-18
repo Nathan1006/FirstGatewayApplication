6e30b610b1746ab2cf4739c2c71a2567f072d1d6
Nathan1006 <nrpetit@outlook.com>
2021-11-18 17:00:57 -0500

    Deleted the app properties file. Attempting to remake it

--- src/main/java/com/careerdevs/APODcontroller.java
+++ src/main/java/com/careerdevs/APODcontroller.java
@@ -1,33 +1,37 @@
 package com.careerdevs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
 @RestController
+@RequestMapping("/apod")
 public class APODcontroller{
-    public static final String My_Api_Key = "lY8deGLA73qHeBSzv5PcDBDOcwAVihUi9RDO1yag";
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/myKey")
+    public String envTest(){
+        return env.getProperty("nasa.key");
+    }
 
     @GetMapping ("/")
     public String rootRoute(){
         return "Welcome Home!";
     }
 
-    @GetMapping ("apod")
+    @GetMapping ("apodInfo")
     public APOD apodInfo(RestTemplate restTemplate){
-        String URL = "https://api.nasa.gov/planetary/apod?api_key=" + My_Api_Key;
+        String URL = "https://api.nasa.gov/planetary/apod?api_key=" + env.getProperty("nasa.key");
 
         APOD apod = restTemplate.getForObject(URL, APOD.class);
 
         return apod;
-    }
-
-    @GetMapping("/apodtest")
-    public String apodTest(){
-
-        return "APOD Testing!";
     }
 
     @PostMapping("/apod")
--- src/main/java/com/careerdevs/Pokemon.java
+++ src/main/java/com/careerdevs/Pokemon.java
@@ -1,8 +1,17 @@
 package com.careerdevs;
 
 public class Pokemon {
+    private int id;
     private String name;
     private String types;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
